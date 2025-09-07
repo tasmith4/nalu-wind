400e0a8368f1422ade9d0771a70c0737ec2e4947
Timothy A. Smith <2timothy18@gmail.com>
2025-09-06 19:23:26 -0600

    fix check in NGPAssembleElemSolver test so it only tests the first addition
    This is because the kernel runs in parallel on GPU, but sequentially on
    CPU, so there is no obvious consistent way to test anything inside the
    kernel. However, smdata only lives inside the kernel, so we can't check
    it outside the kernel either.

--- unit_tests/UnitTestScratchViews.C
+++ unit_tests/UnitTestScratchViews.C
@@ -236,10 +236,8 @@ do_assemble_elem_solver_test(
           stk::simd::get_data(smdata.simdrhs(0),0),
           stk::simd::get_data(smdata.simdrhs(1),0));
 
-      result.d_view(0) =
-        abs(stk::simd::get_data((smdata.simdrhs(0) - 2.0), 0)) < 1.e-9 ? 1 : 0;
-      result.d_view(1) =
-        abs(stk::simd::get_data((smdata.simdrhs(1) - 0.0), 0)) < 1.e-9 ? 1 : 0;
+      result.d_view(0) = (result.d_view(0) == 1 || abs(stk::simd::get_data((smdata.simdrhs(0) - 2.0), 0)) < 1.e-9) ? 1 : 0;
+      result.d_view(1) = (result.d_view(1) == 1 || abs(stk::simd::get_data((smdata.simdrhs(1) - 0.0), 0)) < 1.e-9) ? 1 : 0;
     });
 
   result.modify<IntViewType::execution_space>();
